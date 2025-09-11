f5719b4fcbb9b971c795a36fa87deda1c39afe1e
EmmanuelIbiyemi <ibiyemiemmanuel68@gmail.com>
2025-09-11 15:35:26 +0100

    XBackend

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask , jsonify
+import datetime as date;
+app = Flask(__name__);
+
+startTime: date;
+
+@app.route("/status" , method=["GET"])
+async def status():
+    status={
+        "status":"OK",
+        "version":"1.0",
+        "uptime": str(date.datetime.now() - startTime),
+        "timestamp": str(date.datetime.now())
+    }
+    return jsonify(status)
+
+if ( __name__ == "__main__" ):
+
+    # -- This is to get the time the backend started
+    startTime = date.datetime.now();
+    
+    app.run(debug=True)
+    print("-----XCLONE-backend JUST STARTED------")
