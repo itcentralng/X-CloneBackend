295e91feeefd25ae5342aedec4b85aba5ea43ec9
EmmanuelIbiyemi <ibiyemiemmanuel68@gmail.com>
2025-10-15 15:56:51 +0100

    updated the port issues

--- connection/connect_db.py
+++ connection/connect_db.py
@@ -8,10 +8,10 @@ load_dotenv()
 #-- This is for the getting the connection
 def get_Connection():
         return psycopg2.connect(
-            host=os.getenv("HOST"),
+            host=os.getenv("DBHOST"),
             dbname=os.getenv("DBNAME"),
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            port=os.getenv("PORT"),
+            user=os.getenv("DBUSER"),
+            password=os.getenv("DBPASSWORD"),
+            port=os.getenv("DBPORT"),
             sslmode="require"
         )
