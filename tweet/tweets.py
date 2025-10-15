07864c9bf32664796b9e58f1e0cc2c46757bdf0a
EmmanuelIbiyemi <ibiyemiemmanuel68@gmail.com>
2025-10-15 13:19:55 +0100

    import get connection func to tweet

--- tweet/tweets.py
+++ tweet/tweets.py
@@ -8,16 +8,7 @@ app = Flask(__name__)
 
 load_dotenv()
 #-- This is for the getting the connection
-def get_Connection():
-        return psycopg2.connect(
-            host=str(os.getenv("HOST")),
-            dbname=str(os.getenv("DBNAME")),
-            user=str(os.getenv("USER")),
-            password=os.getenv("PASSWORD"),
-            port=str(os.getenv("PORT")),
-            sslmode="require"
-        )
-
+from connection.connect_db import get_Connection
 conn = get_Connection()
 
 
