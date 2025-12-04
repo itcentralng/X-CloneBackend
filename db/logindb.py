77fabccaee15328e6db29cd08907d811e65944f0
EmmanuelIbiyemi <ibiyemiemmanuel68@gmail.com>
2025-12-04 17:15:42 +0100

    fixed the code also cause the pydantic Basemodel was not active so it gave error when hosting

--- db/logindb.py
+++ db/logindb.py
@@ -1,5 +1,4 @@
 from flask import Flask , request , jsonify , make_response 
-from pydantic import BaseModel
 from functools import wraps
 
 #--- This is for the jwt
--- mailsenpoint/mailsX.py
+++ mailsenpoint/mailsX.py
@@ -1,54 +1,81 @@
 from flask import Flask , request , jsonify
-import requests
+from flask_mailman import Mail , EmailMessage
 import logging
 
 import os
 from dotenv import load_dotenv
 
-logging.basicConfig(level=logging.INFO) # set log level
+
 load_dotenv() # for reading API key from `.env` file.
 
-# Sandbox API URL format: https://api.mailgun.net/v3/sandbox<ID>.mailgun.org/messages
-MAILGUN_API_URL = "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages"
-FROM_EMAIL_ADDRESS = "Sender Name xcloneitcentral@gmail.com"
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.ethereal.email'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'kory.aufderhar@ethereal.email'
+app.config['MAIL_PASSWORD'] = 'SpNhHVqrVaKhzDCZuT'
 
-app = Flask()
 
-@app.route("/reset-password/request", methods=["POST"])
-async def PasswordRequest():
+#-- This is for the mail flask_mailman just as the doc says
+mail = Mail(app)
+
+
+#-- This is for the mail connection it's self
+# connection = mail.get_connection()
+
+# # Manually open the connection
+# connection.open()
+
+
+# @app.route("/reset-password/request", methods=["POST"])
+# async def PasswordRequest():
+#     try:
+        
+
+#         if resp.status_code == 200:  # success
+#             # logging.info(f"Successfully sent an email to '{to_address}' via Mailgun API.")
+#             jsonify ({"Message":f"Successfully sent an email to '{to_address}' via Mailgun API."})
+#         else:  # error
+#             logging.error(f"Could not send the email, reason: {resp.text}")
+
+#     except Exception as ex:
+#         logging.exception(f"Mailgun error: {ex}")
+
+# @app.route("/reset-password/confirm", methods=["POST"])
+# async def PasswordConfirm():
+#     try:
+        
+
+#         if resp.status_code == 200:  # success
+#             # logging.info(f"Successfully sent an email to '{to_address}' via Mailgun API.")
+#             jsonify ({"Message":f"Successfully sent an email to '{to_address}' via Mailgun API."}), 200
+#         else:  # error
+#             logging.error(f"Could not send the email, reason: {resp.text}")
+#             jsonify ({"Message":f"Could not send the email, reason: {resp.text}"}), 500
+
+#     except Exception as ex:
+#         logging.exception(f"Mailgun error: {ex}")
+
+
+@app.route("/sendmail", methods=["GET"])
+def sendingmail():
     try:
-        api_key = os.getenv("MAILGUN_API_KEY")  
-        data = request.get_json()
-        to_address = data.get("to_address")
+        msg = EmailMessage(
+            subject="Hello Ethereal",
+            body="Forgotten password kindly reset your password ussing the link i will provide",
+            to=["andy9@ethereal.email"],
+            from_email="joshon@gmail.com"
+        )
+        msg.send()
+        # if msg.send():
+        #     return jsonify({"status":"successfull", "Message":"Mail sent successfully"}), 200
+        # else: 
+        #     return jsonify({"status":"failed","Message":"Failed to send mail"}), 500
+        return jsonify({"status":"successfull", "Message":"Mail sent successfully"}), 200
+       
+    except Exception as e:
+        return jsonify({"status":"error","Message":f"Error occured: {str(e)}"}), 500
 
-        resp = requests.post(MAILGUN_API_URL, auth=("api", api_key),
-                             data={"from": FROM_EMAIL_ADDRESS,
-                                   "to": to_address, "subject": "Reset Password", "text": "Good Day, Welcome to Reset Password how would you like to me do it"})
-        if resp.status_code == 200:  # success
-            # logging.info(f"Successfully sent an email to '{to_address}' via Mailgun API.")
-            jsonify ({"Message":f"Successfully sent an email to '{to_address}' via Mailgun API."})
-        else:  # error
-            logging.error(f"Could not send the email, reason: {resp.text}")
+if __name__ == "__main__":
+    app.run(debug=True)
 
-    except Exception as ex:
-        logging.exception(f"Mailgun error: {ex}")
-
-@app.route("/reset-password/confirm", methods=["POST"])
-async def PasswordConfirm():
-    try:
-        api_key = os.getenv("MAILGUN_API_KEY")  
-        data = request.get_json()
-        to_address = data.get("to_address")
-
-        resp = requests.post(MAILGUN_API_URL, auth=("api", api_key),
-                             data={"from": FROM_EMAIL_ADDRESS,
-                                   "to": to_address, "subject": "Reset Password", "text": "Good Day, Welcome to Confirm X accoutn how would you like to me do it"})
-        if resp.status_code == 200:  # success
-            # logging.info(f"Successfully sent an email to '{to_address}' via Mailgun API.")
-            jsonify ({"Message":f"Successfully sent an email to '{to_address}' via Mailgun API."}), 200
-        else:  # error
-            logging.error(f"Could not send the email, reason: {resp.text}")
-            jsonify ({"Message":f"Could not send the email, reason: {resp.text}"}), 500
-
-    except Exception as ex:
-        logging.exception(f"Mailgun error: {ex}")
